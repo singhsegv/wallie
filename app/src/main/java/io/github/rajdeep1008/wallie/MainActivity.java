dfb639dd433e1a876edea4bd008b7750e6886629
rajdeep1008 <iamrajdeep1008@gmail.com>
2017-08-20 02:09:35 +0530

    added xchange dependency

--- app/src/main/java/io/github/rajdeep1008/wallie/MainActivity.java
+++ app/src/main/java/io/github/rajdeep1008/wallie/MainActivity.java
@@ -3,6 +3,8 @@ package io.github.rajdeep1008.wallie;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.knowm.xchange.vaultoro.VaultoroExchange;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
